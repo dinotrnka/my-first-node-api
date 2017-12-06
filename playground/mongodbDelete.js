8cf94b57686c27a7cd4091b7685cfa7c9613b5b2
Dino Trnka <dinotrnka@gmail.com>
2017-12-06 20:53:35 +0100

    Added Delete sample code

---
+++ playground/mongodbDelete.js
@@ -0,0 +1,31 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApi', (error, db) => {
+  if (error) {
+    return console.log('Unable to connect to Mongo DB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos')
+  //   .deleteMany({ text: 'Eat ice cream'})
+  //   .then((result) => {
+  //     console.log(result);
+  //   });
+
+  // db.collection('Todos')
+  //   .deleteOne({text: 'Drive Porshe'})
+  //   .then((result) => {
+  //     console.log(result);
+  //   });
+
+  db.collection('Todos')
+    .findOneAndDelete({ 
+      _id: new ObjectID('5a282b55d5724a1d3161d7cc') 
+    })
+    .then((result) => {
+      console.log(result);
+    });
+
+  db.close();
+});
