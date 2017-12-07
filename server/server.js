2fcb074eb7028de267c6b1ba95d2e2281073806d
Dino Trnka <dinotrnka@gmail.com>
2017-12-07 10:34:59 +0100

    Some sexy refactoring

--- server/server.js
+++ server/server.js
@@ -1,52 +1,12 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/Todo');
+var { User } = require('./models/User');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  }, 
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
-});
+var app = express();
 
-// var newTodo = new Todo({
-//   text: 'Wash dishes',
-//   completed: true,
-//   completedAt: 1512593893
-// });
-
-// newTodo.save().then((doc) => {
-//   console.log('Saved Todo', JSON.stringify(doc, undefined, 2));
-// }, (error) => {
-//   console.log('Unable to save Todo', error);s
-// });
-
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  }
-});
-
-var newUser = new User({
-  email: 'kuze@gmail.com'
-});
-
-newUser.save().then(doc => {
-  console.log('Saved user', doc);
-}, error => {
-  console.log('Unable to save user', error);
+app.listen(3000, () => {
+  console.log('Started on port 3000.');
 });
