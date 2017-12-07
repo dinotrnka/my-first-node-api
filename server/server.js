404a22625a22864e82726ef373a11fab70a2655d
Dino Trnka <dinotrnka@gmail.com>
2017-12-07 11:01:55 +0100

    Added POST /todos route

--- server/server.js
+++ server/server.js
@@ -7,6 +7,24 @@ var { User } = require('./models/User');
 
 var app = express();
 
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  console.log(req.body.text);
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo
+    .save()
+    .then((doc) => {
+      res.send(doc);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000.');
 });
