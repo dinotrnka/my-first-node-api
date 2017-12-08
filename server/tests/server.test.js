004ee1047503517ad08b3a5658b16eb47cdbdaa9
Dino Trnka <dinotrnka@gmail.com>
2017-12-09 00:17:52 +0100

    Add tests for POST /users/login route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -215,7 +215,7 @@ describe('POST /users', () => {
           expect(user).toBeTruthy();
           expect(user.password).not.toBe(password);
           done();
-        });
+        }).catch((e) => done(e));
       });
   });
 
@@ -238,6 +238,48 @@ describe('POST /users', () => {
       .post('/users')
       .send({ email, password })
       .expect(400)
+      .end(done);
+  });
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeTruthy();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          // expect(user.tokens[0]).toHaveProperty({
+          //   access: 'auth',
+          //   token: res.headers['x-auth']
+          // });
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: 'hahahaha',
+        password: 'hahaha'
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeFalsy();
+      })
       .end(done);
   });
 });
